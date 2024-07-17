633b397c0cf499da368ec6ed1521cd494a3416a4
Kirill <ananevka@mail.ru>
2024-07-17 16:43:14 +0300

    v.0.1

---
+++ src/main/java/com/lemmiwinks/myscheduleserver/MyScheduleServerApplication.java
@@ -0,0 +1,13 @@
+package com.lemmiwinks.myscheduleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyScheduleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyScheduleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lemmiwinks/myscheduleserver/MyscheduleserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemmiwinks.myscheduleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyscheduleserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
