1776f0cb0b6221f1e785ab2c166bc588ae094637
Kirill <ananevka@mail.ru>
2024-08-15 18:26:50 +0300

    v.4 + faq db (controller, repository, model)

--- src/main/java/com/lemmiwinks/myscheduleserver/controllers/FaqController.java
+++ src/main/java/com/lemmiwinks/myscheduleserver/controllers/FaqController.java
@@ -1,27 +1,31 @@
 package com.lemmiwinks.myscheduleserver.controllers;
 
 import com.lemmiwinks.myscheduleserver.models.FaqModel;
-import com.lemmiwinks.myscheduleserver.models.ProductionCalendarModel;
 import com.lemmiwinks.myscheduleserver.repositiry.FaqRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/faq")
 public class FaqController {
 
     @Autowired
     private FaqRepository faqRepository;
+
     private static final Logger logger = LoggerFactory.getLogger(FaqController.class);
 
-    @GetMapping("/faq")
-    public String faq(Model model) {
+    @GetMapping()
+    public List<FaqModel> faq() {
         logger.info("Вызван метод faqModels");
 
-        Iterable<FaqModel> faqModels = faqRepository.findAll();
+        List<FaqModel> faqModels = faqRepository.findAll();
 
         logger.info(faqRepository.toString());
 
@@ -29,7 +33,6 @@ public class FaqController {
             logger.info(item.toString());
         }
 
-        model.addAttribute("faqModels", faqModels);
-        return "faqModels";
+        return faqModels;
     }
 }
