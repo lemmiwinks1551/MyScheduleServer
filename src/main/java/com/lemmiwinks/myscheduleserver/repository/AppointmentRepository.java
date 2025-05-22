509f38338b916e0676218a377f6c48c687693e86
Kirill <ananevka@mail.ru>
2025-05-22 18:24:39 +0300

    + date sorting fix

--- src/main/java/com/lemmiwinks/myscheduleserver/repository/AppointmentRepository.java
+++ src/main/java/com/lemmiwinks/myscheduleserver/repository/AppointmentRepository.java
@@ -27,5 +27,7 @@ public interface AppointmentRepository extends JpaRepository<Appointment, String
             @Param("userName") String userName
     );
 
-    List<Appointment> findByUserNameAndDeletedFalseOrderByDateDesc(String userName);
+    @Query("SELECT a FROM Appointment a WHERE a.userName = :userName AND a.deleted = false ORDER BY STR_TO_DATE(a.date, '%d.%m.%Y') DESC")
+    List<Appointment> findByUserNameAndDeletedFalseOrderByDateDesc(@Param("userName") String userName);
+
 }
